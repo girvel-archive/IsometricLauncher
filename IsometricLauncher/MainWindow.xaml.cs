0594552cc0ed51acc254065727c32a63e44348a4
girvel <widauka@ya.ru>
2016-10-31 10:07:47 +0500

    Initial

---
+++ IsometricLauncher/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace Isometric.Launcher
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var i = 1;
+            foreach (var screen in Screen.AllScreens)
+            {
+                ScreenComboBox.Items.Add($"Display {i++} {(screen.Primary ? "(Primary)" : "")}");
+            }
+        }
+    }
+}
